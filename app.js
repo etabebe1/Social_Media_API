948d8a4580bc206b7698e3c171079f92c81464ab
etabebe1 <etabebe1@gmail.com>
2023-11-05 04:54:25 -0500

    review previous commit

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const userRouter = require("./routes/user");
 const authRouter = require("./routes/auth");
 const postRouter = require("./routes/post");
 
+const multer = require("multer");
+
 // making donEnv ready to use
 dotenv.config();
 
@@ -21,6 +23,34 @@ app.use(cors());
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common")); // used to indicate request and related info
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+
+  /*// TODO: consider to check the file name if or if not error occurs due to that */ //
+  // if error occurs use the following method to setup filename
+  // filename: (req, file, cb) => {
+  //   cb(null, file.originalname);
+  // },
+
+  filename: (req, file, cb) => {
+    const uniqueSuffix = `${Date.now()}_${Math.round(Math.random() * 1e9)}`;
+    cb(null, uniqueSuffix + "_" + file.originalname);
+  },
+
+  //  file.fieldname + "_" + uniqueSuffix
+});
+
+const upload = multer({ storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploaded successfully.");
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 app.use("/api/users", userRouter);
 app.use("/api/authentication", authRouter);
